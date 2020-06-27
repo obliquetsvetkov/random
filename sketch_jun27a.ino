7569337ab9f5965c8d0989a505826ea5c1245379
obliquetsvetkov <obliquetsvetkov@gmail.com>
2020-06-27 16:26:21 +0100

    Update sketch_jun27a.ino
    fixed a typo; some vague comments made slightly less vague.

--- sketch_jun27a.ino
+++ sketch_jun27a.ino
@@ -1,11 +1,11 @@
 int pwm_pin = 3;
 
 void setup() {
-  analogWriteFrequency(3, 25000); // frequency setup - 11bit sits at 23437.5hz
+  analogWriteFrequency(3, 25000); // frequency setup - 11bit sits at 23437.5hz; close enough.
   analogWriteResolution(11);
   pinMode(pwm_pin, OUTPUT);
 
-  setPwmDuty(768); //33%?
+  setPwmDuty(768); //33%? sufficient for low-moderate loads, quiet too.
 }
 
 void loop() {
@@ -16,9 +16,9 @@ void loop() {
 //    delay(2000);
 //    setPwmDuty(1024); //these are nice clear steps
 //    delay(2000);
-//    setPwmDuty(1535); //0-2048 to adjust duty cycle
+//    setPwmDuty(1535); //0-2047 to adjust duty cycle
 //    delay(2000);    
-//    setPwmDuty(2047); // max power
+//    setPwmDuty(2047); // max power - painfully loud.
 //    delay(2000);
 }
 
